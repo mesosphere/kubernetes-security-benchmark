e67260db28c95d8b353cce9b906aa69064a19378
Jimmi Dyson <jimmidyson@gmail.com>
2018-03-23 17:42:44 +0000

    cis: add version subcommand to output version of Kubernetes CIS Benchmark being tested against

--- cmd/cisversion.go
+++ cmd/cisversion.go
@@ -15,34 +15,23 @@
 package cmd
 
 import (
-	"flag"
-	"testing"
+	"fmt"
 
-	"github.com/onsi/ginkgo/config"
 	"github.com/spf13/cobra"
-
-	"github.com/mesosphere/kubernetes-security-benchmark/pkg/cis"
 )
 
-// cisCmd represents the cis command
-var cisCmd = &cobra.Command{
-	Use:   "cis",
-	Short: "Run Kubernetes CIS Benchmark tests",
-	Long:  `Run Kubernetes CIS Benchmark tests.`,
+const CISVersion = "1.2.0"
+
+// cisVersionCmd represents the cisversion command
+var cisVersionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version of the Kubernetes CIS Benchmark",
+	Long:  `Prints the version of the Kubernetes CIS Benchmark.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		testing.Main(func(pat, str string) (bool, error) { return true, nil },
-			[]testing.InternalTest{{"KubernetesCISBenchmark", cis.CISBenchmark}},
-			[]testing.InternalBenchmark{},
-			[]testing.InternalExample{})
+		fmt.Printf("Kubernetes CIS Benchmark v%s\n", CISVersion)
 	},
 }
 
 func init() {
-
-	ginkgoFlagSet := flag.NewFlagSet("ginkgo", flag.ContinueOnError)
-	config.Flags(ginkgoFlagSet, "ginkgo", false)
-
-	cisCmd.Flags().AddGoFlagSet(ginkgoFlagSet)
-
-	rootCmd.AddCommand(cisCmd)
+	cisCmd.AddCommand(cisVersionCmd)
 }
