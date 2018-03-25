9182a77779298847dff9fb903790ec1fe0ee7910
Jimmi Dyson <jimmidyson@gmail.com>
2018-03-25 22:28:47 +0100

    global: sort out license headers

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Jimmi Dyson <jdyson@mesosphere.com>
+// Copyright © 2018 Jimmi Dyson <jimmidyson@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
